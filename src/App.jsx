cbc346fa6d5ee6f952348299a1d93947da3c2975
Celso Franciscano <celsofranciscano24@gmail.com>
2024-04-26 15:12:13 -0400

    Last app

--- src/App.jsx
+++ src/App.jsx
@@ -13,69 +13,69 @@ function App() {
     <div>
       <Menu />
       <div className="grid gap-8 px-4 md:px-12 pt-20 ">
-        <header className="font-bold">Listas enlazadas</header>
+        {/* <header className="font-bold">Listas enlazadas</header>
 
-        <details open className="rounded-md bg-blue-200 pb-4 px-4">
+        <details  className="rounded-md bg-blue-200 pb-4 px-4">
           <summary className="text-lg font-bold mt-4">
             Ejercicio Nº 1 Listas enlazadas
           </summary>
           <ListaAdd />
-        </details>
-        <details open className="rounded-md bg-blue-200 pb-4 px-4">
+        </details> */}
+        <details  className="rounded-md bg-blue-200 pb-4 px-4">
           <summary className="text-lg font-bold mt-4">
             Ejercicio Nº 1 Listas enlazadas
           </summary>
           <ListaOne />
         </details>
-        <details open className="rounded-md bg-blue-200 pb-4 px-4">
+        <details  className="rounded-md bg-blue-200 pb-4 px-4">
           <summary className="text-lg font-bold mt-4">
             Ejercicio Nº 2 Listas enlazadas
           </summary>
 
           <ListaTwo />
         </details>
-        <details open className="rounded-md bg-blue-200 pb-4 px-4">
+        <details  className="rounded-md bg-blue-200 pb-4 px-4">
           <summary className="text-lg font-bold mt-4">
             Ejercicio Nº 3 Listas enlazadas
           </summary>
           <ListaThree />
         </details>
-        <details open className="rounded-md bg-blue-200 pb-4 px-4">
+        <details  className="rounded-md bg-blue-200 pb-4 px-4">
           <summary className="text-lg font-bold mt-4">
             Ejercicio Nº 1 Pilas
           </summary>
 
           <PilaOne />
         </details>
-        <details open className="rounded-md bg-blue-200 pb-4 px-4">
+        <details  className="rounded-md bg-blue-200 pb-4 px-4">
           <summary className="text-lg font-bold mt-4">
             Ejercicio Nº 2 Pilas
           </summary>
 
           <PilaTwo />
         </details>
-        <details open className="rounded-md bg-blue-200 pb-4 px-4">
+        <details  className="rounded-md bg-blue-200 pb-4 px-4">
           <summary className="text-lg font-bold mt-4">
             Ejercicio Nº 3 Pilas
           </summary>
 
           <PilaThree />
         </details>
-        <details open className="rounded-md bg-blue-200 pb-4 px-4">
+        <details  className="rounded-md bg-blue-200 pb-4 px-4">
           <summary className="text-lg font-bold mt-4">
             Ejercicio Nº 1 Colas
           </summary>
 
           <ColasOne />
         </details>
-        <details open className="rounded-md bg-blue-200 pb-4 px-4">
+        <details  className="rounded-md bg-blue-200 pb-4 px-4">
           <summary className="text-lg font-bold mt-4">
             Ejercicio Nº 2 Colas
           </summary>
 
           <ListaOne />
         </details>
-        <details open className="rounded-md bg-blue-200 pb-4 px-4">
+        <details  className="rounded-md bg-blue-200 pb-4 px-4">
           <summary className="text-lg font-bold mt-4">
             Ejercicio Nº 3 Colas
           </summary>
